350c7a13acefec9f2ec1d602935f05a0e45524e3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-30 21:33:02 +0000

    Deploy KDCinfo/expired-to-be-spa to github.com/KDCinfo/expired-to-be-spa.git:gh-pages

---
+++ precache-manifest.0fe789649840f7de377d2a6542309bc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63fa0cd782d2de1f4671",
+    "url": "/expired-to-be-spa/static/css/main.9b0a9288.chunk.css"
+  },
+  {
+    "revision": "63fa0cd782d2de1f4671",
+    "url": "/expired-to-be-spa/static/js/main.63fa0cd7.chunk.js"
+  },
+  {
+    "revision": "8e93de1fc89ebb7e0148",
+    "url": "/expired-to-be-spa/static/js/1.8e93de1f.chunk.js"
+  },
+  {
+    "revision": "28a894981b6dd2682a46",
+    "url": "/expired-to-be-spa/static/js/runtime~main.28a89498.js"
+  },
+  {
+    "revision": "ea2bdc796120025c49cce85943ab1193",
+    "url": "/expired-to-be-spa/index.html"
+  }
+];
